051564544a15f1c6f96e0d6e144824507751df9d
tauesp127-alexis <gestionti@taurus.com.mx>
2024-10-14 13:43:06 -0600

    Home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { LoaderComponent } from '../../shared/loader/loader.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, LoaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  public isLoading = true
+
+  constructor() {
+    setTimeout(() => {
+      this.isLoading = false;
+    },1500)
+  }
 }
